74b3bcf320b4aa4d48d1272fe893d9ba370362bc
amit <amit@divine-it.net>
2025-04-22 18:07:05 +0600

    Initial Commit

---
+++ Boycott/BoycottApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoycottApp.swift
+//  Boycott
+//
+//  Created by Amit on 22/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BoycottApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoycottTests/BoycottTests.swift
@@ -0,0 +1,16 @@
+//
+//  BoycottTests.swift
+//  BoycottTests
+//
+//  Created by Amit on 22/4/25.
+//
+
+import Testing
+
+struct BoycottTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
